45580e3f87bc861d43ddb4d920d1af971310900c
Lqmnlhkim15 <luqmanlhkim15@gmail.com>
2023-04-04 19:24:14 +0700

    landingpage

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> MindHealthy</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+<!-- NAVBAR / HEADER -->
+<nav class="navbar navbar-expand-lg navbar-light mt-3">
+  <div class="container">
+    <a href="#"><img src="/img/logo.svg"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item me-4 my-2">
+          <a aria-current="page" href="#">Layanan</a>
+        </li>
+        <li class="nav-item me-4 my-2">
+          <a href="#">Test Psikologi</a>
+        </li>
+        <li class="nav-item me-4 my-2">
+          <a href="#">Blog</a>
+        </li>
+        <li class="nav-item me-4 my-2">
+          <a href="#" tabindex="-1" aria-disabled="true">Tentang Kami</a>
+        </li>
+      </ul>
+      <button type="button" id="btn" class="btn ms-auto">Konsultasi Sekarang</button>
+    </div>
+  </div>
+</nav>
+
+<!-- CONTENT -->
+<div class="container">
+  <div class="mulai">
+    <h5>Ceritakan masalah kamu dengan ahlinya</h5>
+    <h3>Selalu ada pagi yang damai <br> setelah malam yang penuh <br> badai. </h3>
+    <button type="button" class="btn btn-sm" id="btn">Daftar Sekarang</button>
+    <button type="button" class="btn btn-sm" id="btn2">Tentang Kami</button>
+  </div>
+</div>
+<div class="teks container">
+  <h4 class="mb-5">MindHealty ingin membantu anda <br> dalam mengurangi masalah yang <br> anda alami</h4>
+</div>
+
+<!-- CARD -->
+<div class="card container mt-5">
+  <div class="row">
+    <div class="col-4 text-center">
+      <img src="/img/card1.png" alt="">
+      <h3 class="mt-4">Mental Block</h3>
+    </div>
+    <div class="col-4 text-center">
+      <img src="/img/card2.png" alt="">
+      <h3 class="mt-4">Anti Sosial</h3>
+    </div>
+    <div class="col-4 text-center">
+      <img src="/img/card3.png" alt="">
+      <h3 class="mt-4">Depresi</h3>
+    </div>
+  </div>
+</div>
+
+<!-- Layanan -->
+<div class="layanan container">
+  <div class="layanan row">
+    <div class="col-5">
+      <img src="/img/layanan.png" alt="">
+    </div>
+    <div class="col-7">
+      <h1>Layanan Lain Kami</h1>
+      <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. <br> Velit officia consequat duis enim velit mollit. Exercitation veniam consequat <br> sunt nostrud amet.</p>
+      <div class="list-layanan">
+        <h3>Mentoring</h3>
+        <img src="/img/arrowdown.png" alt="">
+        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia</br>consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+        <button type="button" class="btn btn-sm" id="btn">Selengkapnya</button>
+      </div>
+      <!-- <div class="list-layanan">
+        <h3>Konseling</h3>
+        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia</br>consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+      </div>
+      <div class="list-layanan">
+        <h3>Kelas Online</h3>
+        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia</br>consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+      </div> -->
+    </div>
+  </div>
+</div>
+
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
